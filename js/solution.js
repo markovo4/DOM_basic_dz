6a962218eade59ec8ee191cf0449e1fea8e77361
markovo4 <obf2007@gmail.com>
2024-02-29 16:33:11 +0200

    DOM-Homework-1

--- js/solution.js
+++ js/solution.js
@@ -1,22 +1,21 @@
 (function () {
-  const bodyChildren = document.body.childNodes;
-  let sum = 0;
+    const bodyChildren = document.body.childNodes;
+    let sum = 0;
 
-  const ul = bodyChildren[1];
+    const ul = bodyChildren[1];
 
-  for (const element of ul.childNodes) {
-    console.log(element);
-    sum += 1;
-  }
-  console.log(sum);
+    for (const element of ul.childNodes) {
+        console.log(element);
+        sum += 1;
+    }
+    console.log(sum);
 
-  const arr = Array.from(ul.childNodes);
+    const arr = Array.from(ul.childNodes);
 
-  const filteredArr = arr.filter((item) => item.nodeName === 'LI');
-
-  const textArr = [];
-  for (const element of filteredArr) {
-    textArr.push(element.textContent);
-  }
-  console.log(textArr);
+    const filteredArr = arr.filter((item) => item.nodeName === 'LI');
+    const textArr = [];
+    for (const element of filteredArr) {
+        textArr.push(element.textContent);
+    }
+    console.log(textArr);
 }());
