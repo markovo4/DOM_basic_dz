c6b436c18da3aca4d42b52df02f0df5b11d61da5
markovo4 <obf2007@gmail.com>
2024-05-13 11:28:02 +0300

    hw-DOM-17

---
+++ js/photos.js
@@ -0,0 +1,31 @@
+class Gallery {
+  div = null;
+
+  constructor() {
+    this.div = document.querySelector('.d-flex');
+  }
+
+  get getDiv() {
+    return this.div;
+  }
+
+  async logPhotos() {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos${window.location.search}`);
+      const photos = await response.json();
+      return photos;
+    } catch (error) {
+      throw new Error(`Error fetching photos: ${error}`);
+    }
+  }
+}
+
+const newGallery = new Gallery();
+const div = newGallery.getDiv;
+newGallery.logPhotos().then((photos) => {
+  photos.forEach((photo) => {
+    const img = document.createElement('img');
+    img.src = photo.thumbnailUrl;
+    div.appendChild(img);
+  });
+});
--- js/solution.js
+++ js/solution.js
@@ -1,54 +1,46 @@
-const ul = document.querySelector('.list-group');
+class Album {
+  #req = null;
 
-if (ul) {
-  const logAlbums = async function () {
+  constructor(req) {
+    this.#req = req;
+    this.ul = document.querySelector('.list-group');
+    this.ul.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  get getUl() {
+    return this.ul;
+  }
+
+  async logAlbum() {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+      const response = await fetch(this.#req);
       const albums = await response.json();
       return albums;
     } catch (error) {
       throw new Error(`Error fetching albums: ${error}`);
     }
-  };
+  }
 
-  logAlbums().then((albums) => {
-    albums.forEach((album) => {
-      const li = document.createElement('li');
-
-      li.setAttribute('id', `${album.id}`);
-      li.textContent = `${album.id}. ${album.title}`;
-      li.className = 'list-group-item';
-
-      ul.appendChild(li);
-    });
-  }).catch((error) => {
-    throw new Error(`Error in promise chain: ${error}`);
-  });
-
-  ul.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = `./photos.html?albumId=${e.target.id}`;
-  });
-}
-
-if (window.location.search.includes('?albumId=')) {
-  const id = window.location.search[window.location.search.length - 1];
-  const div = document.querySelector('.d-flex');
-  const logPhotos = async function () {
-    try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/photos${window.location.search}`);
-      const photos = await response.json();
-      return photos;
-    } catch (error) {
-      throw new Error(`Error fetching photos: ${error}`);
+  handleClick(e) {
+    const albumId = e.target.id;
+    if (albumId) {
+      window.location.href = `./photos.html?albumId=${albumId}`;
     }
-  };
-
-  logPhotos(id).then((photos) => {
-    photos.forEach((photo) => {
-      const img = document.createElement('img');
-      img.src = photo.thumbnailUrl;
-      div.appendChild(img);
-    });
-  });
+  }
 }
+
+const newAlbum = new Album('https://jsonplaceholder.typicode.com/albums');
+const ul = newAlbum.getUl;
+newAlbum.logAlbum().then((albums) => {
+  albums.forEach((album) => {
+    const li = document.createElement('li');
+
+    li.setAttribute('id', `${album.id}`);
+    li.textContent = `${album.id}. ${album.title}`;
+    li.className = 'list-group-item';
+
+    ul.appendChild(li);
+  });
+}).catch((error) => {
+  throw new Error(`Error in promise chain: ${error}`);
+});
